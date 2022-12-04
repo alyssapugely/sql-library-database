7d900162f407c48b5557af6402bf565167fb6309
alyssapugely <106921279+alyssapugely@users.noreply.github.com>
2022-12-03 19:21:32 -0500

    Delete OldestYoungestClients.sql

--- OldestYoungestClients.sql
+++
@@ -1,5 +0,0 @@
-/* Displays fields ‘OldestClient’ and ‘YoungestClient’, with records depicting their age. Age calculated by subtracting the min and max birth years from 2022, the current year */
-SELECT MAX(2022 - ClientDoB) AS OldestClient, MIN(2022 - ClientDoB) AS YoungestClient
-FROM Client
-INNER JOIN Borrower
-ON Borrower.ClientId = Client.ClientId;
